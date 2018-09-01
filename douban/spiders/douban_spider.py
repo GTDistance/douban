550cfa84667a435d76924e1f72bf6e83a169b2a6
thomas <thomas@github.com>
2018-09-01 17:01:29 +0800

    Initial commit

---
+++ douban/spiders/douban_spider.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from douban.items import DoubanItem
+
+
+class DoubanSpiderSpider(scrapy.Spider):
+    # 爬虫名
+    name = 'douban_spider'
+    # 允许的域名
+    allowed_domains = ['movie.douban.com']
+    # 入口url，扔到调度器里去
+    start_urls = ['https://movie.douban.com/top250']
+
+    # 默认的解析方法
+    def parse(self, response):
+        # print (response.text)
+        movie_list = response.xpath("//div[@class='article']//ol[@class='grid_view']/li")
+        for i_item in movie_list:
+            douban_item = DoubanItem()
+            douban_item['serial_number'] = i_item.xpath(".//div[@class='item']//em/text()").extract_first()
+            douban_item['movie_name'] = i_item.xpath(
+                ".//div[@class='info']/div[@class='hd']/a/span[1]/text()").extract_first()
+            content = i_item.xpath(".//div[@class='info']/div[@class='bd']/p[1]/text()").extract()
+            # douban_item['introduce'] = i_item.xpath(".//div[@class='info']/div[@class='bd']/p[1]/text()")
+            for i_content in content:
+                content_s = "".join(i_content.split())
+                douban_item['introduce'] = content_s
+            douban_item['star'] = i_item.xpath(
+                ".//div[@class='info']/div[@class='bd']/div[@class='star']/span[2]/text()").extract_first()
+            douban_item['evaluate'] = i_item.xpath(
+                ".//div[@class='info']/div[@class='bd']/div[@class='star']/span[4]/text()").extract_first()
+            douban_item['describe'] = i_item.xpath(
+                ".//div[@class='info']/div[@class='bd']/p[@class='quote']/span/text()").extract_first()
+            # 整到管道
+            yield douban_item
+        # 解析下一页
+        next_link = response.xpath("//span[@class='next']/link/@href").extract()
+        if next_link:
+            next_link = next_link[0]
+            yield scrapy.Request("https://movie.douban.com/top250"+next_link, callback=self.parse)
